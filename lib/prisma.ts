106499f004787652f56a3b0766a000612d6a16ac
Sujal Raj <sujalraj2064@gmail.com>
2026-03-25 21:03:37 +0530

    chore: update dependencies and adjust Prisma configuration
    - Updated @prisma/client and prisma to version 7.5.0
    - Replaced @prisma/client with @prisma/adapter-pg in dependencies
    - Added dotenv as a new dependency
    - Removed DATABASE_URL from the Prisma schema datasource configuration

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("Missing DATABASE_URL environment variable");
+}
+
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({
-     datasourceUrl: process.env.DATABASE_URL,
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    }),
   });
 
 if (process.env.NODE_ENV !== "production") {
